023f2fba9814d66a54fe2bb0b9f3472301af8120
mag10078 <105074976+mag10078@users.noreply.github.com>
2022-05-27 23:21:10 +0530

    Add files via upload

---
+++ output assignment 4.py
@@ -0,0 +1,30 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/madhu/OneDrive/Desktop/python assignment .4.py =======
+Please enter your marks 56
+Your final grade is C
+Please enter year: 2026
+The year  2026  is not a leap year :(
+Question: 2 x 2 = 4
+Right!
+Question: 2 x 0 = 0
+Right!
+Question: 1 x 1 = 10
+Wrong. The answer is  1
+Question: 7 x 3 = 21
+Right!
+Question: 0 x 6 = 89
+Wrong. The answer is  0
+Question: 0 x 10 = 10
+Wrong. The answer is  0
+Question: 2 x 4 = 8
+Right!
+Question: 3 x 6 = 18
+Right!
+Question: 5 x 0 = 0
+Right!
+Question: 6 x 0 = 0
+Right!
+Answer: 177
+
---
+++ python assignment .4.py
@@ -0,0 +1,53 @@
+#Question 1
+
+grade= float(input("Please enter your marks "))
+
+if grade<25:
+    print("Your final grade is F")
+elif grade>=25 and grade<45:
+    print("Your final grade is E")
+elif grade>=45 and grade<50:
+    print("Your final grade is D")
+elif grade>=50 and grade<60:
+    print("Your final grade is C")
+elif grade>=60 and grade<80:
+    print("Your final grade is B")
+elif grade>=80 and grade<=100:
+    print("Your final grade is A")
+else:
+    print("Error!!")
+    
+    
+#Question 2
+
+yr= int(input("Please enter year: "))
+
+if yr % 100 == 0:
+    if yr % 400 == 0:
+        print("The year ",yr," is a leap year!")
+    else:
+        print("The year ",yr," is not a leap year :(")
+elif yr % 4 == 0:
+    print("The year ",yr," is a leap year!")
+else:
+    print("The year ",yr," is not a leap year :(")
+
+
+#Question 3
+
+from random import randint
+
+for loop_variable in range(10):
+    n1 = randint(0,10)
+    n2 = randint(0,10)
+    if int(input('Question: ' + str(n1) + ' x ' + str(n2) + ' = ')) == n1 * n2: print('Right!')
+    else: print('Wrong. The answer is ', n1 * n2)
+
+
+#Question 4
+
+num=1
+while num<200:
+    if num%5 == 2 and num%6 == 3 and num%7 == 2:
+        print("The number is: " + str(num))
+    num = num + 1
